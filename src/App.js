5dba697857a39391df205da0f9accc856b413f6e
Tiago Luvizoto Neves <contato@tlndesign.com.br>
2024-04-03 06:50:38 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Contador: {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCount(count + 1)}>
+          Aumentar 
+        </button>
+        <button onClick={() => setCount(count - 1)}>
+          Diminuir
+        </button>
       </header>
     </div>
   );
